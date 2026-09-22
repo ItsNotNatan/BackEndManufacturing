27db108396fb3afb79f8208f424febdf066a5c61
Natan Guimarães dos Santos <natanguimaraes158@gmail.com>
2026-09-22 08:45:16 -0300

    Add files via upload

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,14 +1,14 @@
 // ==========================================
-// FILE: src/config/corsOptions.js
+// FILE: src/config/corsOptions.js (BACK-END)
 // ==========================================
 
-// Lista de origens permitidas lidas diretamente das variáveis de ambiente (.env)
 const origensPermitidas = [
     process.env.CLIENT_URL || 'http://localhost:5175',
     process.env.LOGISTICS_URL || 'http://localhost:5173',
+    'http://localhost:5174', // <-- Adicionámos a porta fixa da Logística!
     'http://127.0.0.1:5175',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174'  // <-- Adicionámos o IP correspondente
 ];
 
-// Exporta o array para ser utilizado no app.js e no server.js
 module.exports = origensPermitidas;
